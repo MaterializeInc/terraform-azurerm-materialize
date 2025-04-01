00679f3242674e8b8cd46798950a9b51dfcbd009
Justin Bradfield <justin@materialize.com>
2025-03-31 19:43:16 -0500

    add instance_resource_ids to base module outputs

--- outputs.tf
+++ outputs.tf
@@ -96,3 +96,18 @@ output "load_balancer_details" {
     }
   }
 }
+
+output "operator" {
+  description = "Materialize operator details"
+  value = var.install_materialize_operator ? {
+    namespace             = module.operator[0].operator_namespace
+    release_name          = module.operator[0].operator_release_name
+    release_status        = module.operator[0].operator_release_status
+    instances             = module.operator[0].materialize_instances
+    namespace             = module.operator[0].operator_namespace
+    release_name          = module.operator[0].operator_release_name
+    release_status        = module.operator[0].operator_release_status
+    instances             = module.operator[0].materialize_instances
+    instance_resource_ids = module.operator[0].materialize_instance_resource_ids
+  } : null
+}
