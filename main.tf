c25960bfab7bf7bdde30e1d978f6dbbe3c4a53ea
Dennis Felsing <dennis@felsing.org>
2026-07-08 10:59:23 +0000

    Bump to terraform-helm-materialize v0.1.74

--- main.tf
+++ main.tf
@@ -223,7 +223,7 @@ locals {
 }
 
 module "operator" {
-  source = "github.com/MaterializeInc/terraform-helm-materialize?ref=v0.1.73"
+  source = "github.com/MaterializeInc/terraform-helm-materialize?ref=v0.1.74"
 
   count = var.install_materialize_operator ? 1 : 0
 
